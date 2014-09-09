6a42e930728ffd1f8c14baa8741f76363f4d65d8
JL06 <jl_hl@hotmail.com>
2014-09-09 11:21:48 -0500

    llave para sesión

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'Q2RiTTXv0Vx02xY8EpZv52Ny2l4J07N3';
 
 /*
 |--------------------------------------------------------------------------
