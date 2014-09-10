a804a1067d217327dcacb418fabfef31bd5c4f2e
AnGlez <anaglezr13@gmail.com>
2014-09-10 00:18:24 -0500

    registrar venta listo

--- application/views/venta_form.php
+++ application/views/venta_form.php
@@ -7,7 +7,7 @@
           <form name="venta" class="form-horizontal style-form" method="post" action=<?php echo site_url("ventas/insertar_venta") ?>>
            
            <div class="form-group">
-             <label class="control-label col-md-3">Producto</label>
+             <label class="control-label col-md-2">Producto</label>
              <div class="col-md-5">
                <select name="idProducto" class="form-control">
                  <?php if($productos != NULL):?>
@@ -21,35 +21,43 @@
           </div>
 
           <div class="form-group">
-           <label class="control-label col-md-3">Cantidad</label>
-           <input type="number" min="1" name="cantidad" required>
+           <label class="control-label col-md-2">Cantidad</label>
+           <div class="col-md-2">
+             <input type="number" min="1" name="cantidad" required class="form-control">           
+           </div>
          </div>
          
          <div class="form-group">
-          <label class="control-label col-md-3">Vendedor</label> 
-          <select name="idVendedor">
-           <?php if($vendedor != NULL):?>
-            <?php  foreach ($vendedor as $v):?>
-              <option value="<?php echo $v['id'] ?>"><?php echo $v['nombre'] ?></option>
-            <?php endforeach;?>
-          <?php endif; ?>
-        </select>
+          <label class="control-label col-md-2">Vendedor</label> 
+          <div class="col-md-5">
+            <select name="idVendedor" class="form-control">
+             <?php if($vendedor != NULL):?>
+              <?php  foreach ($vendedor as $v):?>
+                <option value="<?php echo $v['id'] ?>"><?php echo $v['nombre'] ?></option>
+              <?php endforeach;?>
+            <?php endif; ?>
+          </select>
+            
+          </div>
       </div>
      
       <div class="form-group">
-        <label class="control-label col-md-3">Lugar</label>
-        <select name="idLugar">
-         <?php if($lugar != NULL):?>
-          <?php foreach ($lugar as $l):?>
-            <option value="<?php echo $l['id'] ?>"><?php echo $l['nombre'] ?></option>
-          <?php endforeach;?>
-        <?php endif; ?>
+        <label class="control-label col-md-2">Lugar</label>
+        <div class="col-md-5">
+          <select name="idLugar" class="form-control">
+           <?php if($lugar != NULL):?>
+            <?php foreach ($lugar as $l):?>
+              <option value="<?php echo $l['id'] ?>"><?php echo $l['nombre'] ?></option>
+            <?php endforeach;?>
+          <?php endif; ?>
 
-      </select>
+        </select>
+          
+        </div>
     </div>
     <div class="form-group">
-      <label class="control-label col-md-3">Fecha</label>
-      <div class="col-md-3 col-xs-11">
+      <label class="control-label col-md-2">Fecha</label>
+      <div class="col-md-5 col-xs-11">
         <div data-date-viewmode="years" data-date-format="dd-mm-yyyy" data-date="01-01-2014" class="input-append date dpYears">
         <input name="fecha" class="form-control form-control-inline input-medium default-date-picker" required size="16" type="text" value="<?php echo date('d-m-y') ?>" >
         </div>
