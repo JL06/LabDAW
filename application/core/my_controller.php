41fed3b7d720e2a88ef2da1b47c6e07229a5550e
lalaufresa <lalaufresa@gmail.com>
2014-10-06 07:38:31 -0500

    no me dejó pullear sin pushear esto

--- application/core/my_controller.php
+++ application/core/my_controller.php
@@ -5,6 +5,8 @@ class MY_Controller extends CI_Controller{
 	{
 		parent::__construct();
 		$this->comprueba_sesion();
+		$this->comprueba_permiso();
+
 	}
 	protected function comprueba_sesion() {
 		if ($this->session->userdata('id') === FALSE) {
@@ -13,13 +15,25 @@ class MY_Controller extends CI_Controller{
 			die();
 		}
 	}
+	
+	protected function comprueba_permiso(){
+		$this->load->model("permission_model");
+		$accion=$this->uri->segment(1)."/".$this->uri->segment(2);
+		$rol=$this->session->userdata('rol');
+		if (!$this->permission_model->has_permission($rol, $accion) && $accion != "/"){
+			redirect("sesion/acceso_denegado");			
+		}else{
+			//$this->log();
+		}
+	}
+
 	protected function validate_form($rules,$form_values,$entity){
 		$valid=1;
 		$unique_error="";
 		foreach ($rules as $r) {
 			if($r['rules'] == 'unique'){
 				$rep=$this->productos_model->repite($entity,$r['field'], $form_values[$r['field']] );
-				
+
 				if ($rep){
 					$unique_error='El '.$r['label'].' no está disponible';
 					$valid=false;
@@ -37,6 +51,15 @@ class MY_Controller extends CI_Controller{
 			$valid.=$unique_error;
 
 		return $valid;
+	}
+
+	protected function log(){
+		$user=$this->session->userdata('id');
+		$date = date("Y-m-d");
+		$accion=$this->uri->segment(1)."/".$this->uri->segment(2);
+		if($accion == "/")
+			$accion="inicio";
+		$this->permission_model->crear("log",array("accion"=>$accion, "idUsuario"=>$user,"fecha"=>$date));
 	}
 }
 ?>
